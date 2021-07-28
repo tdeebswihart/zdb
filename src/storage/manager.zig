6c64be068d75bb177ee00b0a214bb5b0d4190e97
Tim Deeb-Swihart <tim@deebswih.art>
2021-07-28 12:07:04 -0700

    a bit of cleanup

--- src/storage/manager.zig
+++ src/storage/manager.zig
@@ -3,21 +3,21 @@ const block = @import("block.zig");
 const Entry = @import("entry.zig").Entry;
 
 // Storage managers manage access to individual files.
-// Each file is carved up into blocks which we treat as a generic heap.
-// TODO track occupancy using an occupancy map rather than a list of blocks.
+// Each file is carved up into pages which we treat as a generic heap.
+// TODO track occupancy using an occupancy map rather than a list of pages.
 // This manager will never reclaim space.
 //
 // FILE HEADER
 // - u16 block size
-// - u16 number of blocks
-// - number of blocks bytes for the occupancy map
+// - u16 number of pages
+// - number of pages bytes for the occupancy map
 
 pub const FileHeader = struct {
     block_size: u16 = 0,
-    allocated_blocks: u64 = 0,
+    allocated_pages: u64 = 0,
 };
 
-pub const GetError = error {
+pub const Error = error {
     PageDoesntExist,
 };
 
@@ -29,13 +29,23 @@ pub fn Manager(comptime T: type) type {
         file: T,
         file_size: u64,
         mem: *std.mem.Allocator,
-        blocks: std.ArrayList(*PageT),
+        // TODO: cache pages in memory instead of reading and writing constantly. Performance
+        // optimization
+        pages: std.ArrayList(*PageT),
 
         const Self = @This();
 
+
+        fn findBlock(self: anytype, entry: Entry) callconv(.Inline) !*PageT {
+            if (entry.block > self.pages.items.len) {
+                return Error.PageDoesntExist;
+            }
+            return self.pages.items[entry.block];
+        }
+
         pub fn init(file: T, block_size: u16, mem: *std.mem.Allocator) !*Self {
             var hdr = FileHeader{.block_size = block_size};
-            var blocks = std.ArrayList(*PageT).init(mem);
+            var pages = std.ArrayList(*PageT).init(mem);
             var file_size: u64 = 0;
 
             const size = try file.size();
@@ -50,14 +60,14 @@ pub fn Manager(comptime T: type) type {
                 _ = try file.read(std.mem.asBytes(&hdr));
                 std.debug.assert(hdr.block_size == block_size);
 
-                try blocks.resize(hdr.allocated_blocks);
+                try pages.resize(hdr.allocated_pages);
 
                 var offset: u64 = @sizeOf(FileHeader);
                 var i: usize = 0;
-                const sz = @sizeOf(FileHeader) + block_size * hdr.allocated_blocks;
+                const sz = @sizeOf(FileHeader) + block_size * hdr.allocated_pages;
                 while (offset < sz) : (offset += block_size) {
                     const b= try PageT.init(file, offset, block_size, mem);
-                    blocks.items[i] = b;
+                    pages.items[i] = b;
                     i += 1;
                 }
             }
@@ -66,42 +76,42 @@ pub fn Manager(comptime T: type) type {
             mgr.file = file;
             mgr.file_size = file_size;
             mgr.mem = mem;
-            mgr.blocks = blocks;
+            mgr.pages = pages;
 
             return mgr;
         }
 
         pub fn deinit(self: *Self) !void {
-            std.debug.assert(self.header.allocated_blocks == self.blocks.items.len);
+            std.debug.assert(self.header.allocated_pages == self.pages.items.len);
             try self.file.seekTo(0);
             try self.file.writeAll(std.mem.asBytes(&self.header));
-            for (self.blocks.items) |b| {
+            for (self.pages.items) |b| {
                 try b.deinit();
             }
-            self.blocks.deinit();
+            self.pages.deinit();
             self.mem.destroy(self);
         }
 
         pub fn put(self: *Self, record: []const u8) !Entry {
             var i: u64 = 0;
-            var end = self.blocks.items.len;
+            var end = self.pages.items.len;
             while (i < end) : (i += 1) {
-                const b = self.blocks.items[i];
+                const b = self.pages.items[i];
                 if (b.can_contain(@intCast(u16, record.len))) {
                     break;
                 }
             }
             const bl = blk: {
                 if (i == end) {
-                    // No blocks found, alloc a new one
+                    // No pages found, alloc a new one
                     const b = try PageT.init(self.file, self.file_size, self.header.block_size, self.mem);
                     self.file_size += self.header.block_size;
                     try self.file.extend(self.file_size);
-                    try self.blocks.append(b);
-                    self.header.allocated_blocks += 1;
+                    try self.pages.append(b);
+                    self.header.allocated_pages += 1;
                     break :blk b;
                 }
-                break :blk self.blocks.items[i];
+                break :blk self.pages.items[i];
             };
             // TODO if block is out of space update occupancy and make a new one
             var entry = try bl.put(record);
@@ -110,19 +120,12 @@ pub fn Manager(comptime T: type) type {
         }
 
         pub fn get(self: *Self, entry: Entry) ![]const u8 {
-            if (entry.block > self.blocks.items.len) {
-                return GetError.PageDoesntExist;
-            }
-            const b = self.blocks.items[entry.block];
+            const b = try findBlock(self, entry);
             return b.get(entry.slot);
         }
 
         pub fn update(self: *Self, oldEntry: Entry, record: []const u8) !void {
-            const data = try b.get(entry);
-            if (data.len != record.len) {
-                return UpdateEError.InvalidLength;
-            }
-            const bl = self.blocks.items[oldEntry.block];
+            const bl = try findBlock(self, entry);
             try bl.delete(oldEntry.slot);
             var entry = try bl.put(record);
             entry.block = entry.block;
@@ -130,7 +133,8 @@ pub fn Manager(comptime T: type) type {
         }
 
         pub fn delete(self: *Self, entry: Entry) !void {
-            @panic("FIXME: implement delete");
+            const bl = try findBlock(self, entry);
+            try bl.delete(oldEntry.slot);
         }
     };
 }
